3050b82df9cbe3d358ce4742c51bacf75118cd3e
addshore <addshorewiki@gmail.com>
2015-11-17 09:49:06 +0100

    Add dispatch tracking script
    Change-Id: Ia9869c63b6d6ec0802e240ac5136e01daec084e7

---
+++ graphite/dispatch.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Track Wikidata dispatch lag in graphite
+ *
+ * @author Addshore
+ */
+
+$opts = array(
+	'http' => array(
+		'method' => "GET",
+		'header' => "User-Agent: Wikidata-Graphite-Dispatch-Tracking\r\n",
+	),
+);
+
+$context = stream_context_create( $opts );
+
+$json =
+	file_get_contents(
+		'https://www.wikidata.org/w/api.php?action=query&meta=siteinfo&format=json&siprop=statistics',
+		false,
+		$context
+	);
+$json = json_decode( $json, true );
+$json = $json['query']['statistics']['dispatch'];
+
+$stats = array();
+$stats['freshest.pending'] = $json['freshest']['pending'];
+$stats['freshest.lag'] = $json['freshest']['lag'];
+$stats['median.pending'] = $json['median']['pending'];
+$stats['median.lag'] = $json['median']['lag'];
+$stats['stalest.pending'] = $json['stalest']['pending'];
+$stats['stalest.lag'] = $json['stalest']['lag'];
+$stats['average.pending'] = $json['average']['pending'];
+$stats['average.lag'] = $json['average']['lag'];
+
+foreach ( $stats as $name => $value ) {
+	exec( "echo \"wikidata.dispatch.$name:$value|g\" | nc -w 1 -u statsd.eqiad.wmnet 8125" );
+}
