b3bdea008dc4d61c4840baaaf8521d1847fafdb1
addshore <addshorewiki@gmail.com>
2015-11-18 17:20:58 +0100

    Track propertycreators and bots
    Change-Id: Ib6d7115e35ced30a5f9cbfa012b3f20663d136c5

---
+++ graphite/site_stats/bots.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# @author Addshore
+#
+# The number of users in the bots on a give day.
+# Generated using the user_groups table.
+#
+# SELECT ug_group, count(*) AS count FROM user_groups GROUP BY ug_group;
+
+value=$(mysql --defaults-file=/etc/mysql/conf.d/analytics-research-client.cnf -h analytics-store.eqiad.wmnet -A -se "select count(*) as count from user_groups where ug_group = 'bot' group by ug_group" wikidatawiki)
+
+echo "daily.wikidata.site_stats.user_groups.bots $value `date +%s`" | nc -q0 graphite.eqiad.wmnet 2003
