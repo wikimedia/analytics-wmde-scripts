2e7da506b69792f79d72c384790db308fbd9cd47
addshore <addshorewiki@gmail.com>
2015-11-23 14:57:15 +0100

    Fix missing space in db query
    Change-Id: I8fab808f589f56ff4783e0d53c5370a9a9c1931c

--- graphite/site_stats/pages_by_namespace.php
+++ graphite/site_stats/pages_by_namespace.php
@@ -44,7 +44,7 @@ class WikidataPagesByNamespace{
 	private function getSql() {
 		return "SELECT page_namespace AS namespace, page_is_redirect AS redirect, count(*) AS count " .
 			"FROM wikidatawiki.page " .
-			"WHERE page_namespace = 0 OR page_namespace = 120 OR page_namespace = 1" .
+			"WHERE page_namespace = 0 OR page_namespace = 120 OR page_namespace = 1 " .
 			"GROUP BY page_namespace, page_is_redirect";
 	}
 
