979f64cb1e6d149eda61ae654c5f94277ce62b72
addshore <addshorewiki@gmail.com>
2015-12-15 14:16:02 +0000

    Send wd-analysis ref output to graphite
    Change-Id: Ifed872fa9d0fb9e388016dc2276e970e900e8958

---
+++ graphite/wikidata-analysis/reference.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php
+<?php
+
+/**
+ * @author Addshore
+ *
+ * This makes an assumption that the data directory is ~/wikidata-analysis/data
+ * See https://github.com/wmde/wikidata-analysis
+ */
+
+//TODO this will not work on windows (but that is not our target)
+//http://stackoverflow.com/questions/1894917/how-to-get-the-home-directory-from-a-php-cli-script
+$dataDir = $_SERVER['HOME'] . '/wikidata-analysis/data/';
+//Make sure the output dir exists
+if ( !file_exists( $dataDir ) ) {
+	throw new Exception( "Data directory does not exist" );
+}
+
+//Get all the output directories
+$dirs = array_filter( glob( $dataDir . '/*' ), 'is_dir' );
+sort( $dirs );
+$dirs = array_reverse( $dirs );
+
+//Only get the last 10 dumps
+$dirs = array_slice( $dirs, 0, 10 );
+if( count( $dirs ) <= 1 ) {
+	echo "Not many output dirs found!\n";
+}
+
+//Get the outputs and send to graphite
+foreach( $dirs as $dir ) {
+	$dir = rtrim( $dir, '\/' );
+	$dirParts = explode( '/', $dir );
+	$date = array_pop( $dirParts );
+
+	$file = $dir . '/reference.json';
+	if( !file_exists( $file ) ) {
+		echo 'File not found: ' . $file . "\n";
+		continue;
+	}
+
+	$data = json_decode( file_get_contents( $file ), true );
+	foreach( $data as $name => $value ) {
+		$metricName = "daily.wikidata.datamodel.$name";
+		exec( "echo \"$metricName $value `date -d \"$date\" +%s`\" | nc -q0 graphite.eqiad.wmnet 2003" );
+	}
+
+}
