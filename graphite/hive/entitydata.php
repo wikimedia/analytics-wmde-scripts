bfe5a6d1e61c098a1677e5d7b1102d6f97becf92
addshore <addshorewiki@gmail.com>
2015-12-23 11:02:36 +0000

    Make it easy to run entitydata script for past days
    Change-Id: I804f9dea7d901088ef41895364b2220e5a1a0d98

--- graphite/hive/entitydata.php
+++ graphite/hive/entitydata.php
@@ -1,16 +1,23 @@
 #!/usr/bin/php
 <?php
 
+if ( array_key_exists( 1, $argv ) ) {
+	$daysAgo = $argv[1];
+	echo "Getting data from $daysAgo days ago\n";
+} else {
+	$daysAgo = 1;
+}
+
 $dateYesterday = new DateTime();
-$dateYesterday->modify( '-24 hours' );
+$dateYesterday->modify( '-' . $daysAgo . ' day' );
 
 $query = file_get_contents( __DIR__ . '/hql/entitydata.hql' );
 $query = str_replace( '<<YEAR>>', date( 'Y', $dateYesterday->getTimestamp() ), $query );
 $query = str_replace( '<<MONTH>>', date( 'n', $dateYesterday->getTimestamp() ), $query );
 $query = str_replace( '<<DAY>>', date( 'j', $dateYesterday->getTimestamp() ), $query );
 
-$outputFile = __DIR__ . '/entitydata_result.txt';
-$errorFile = __DIR__ . '/entitydata_errors.txt';
+$outputFile = __DIR__ . "/entitydata_result_$daysAgo.txt";
+$errorFile = __DIR__ . "/entitydata_errors_$daysAgo.txt";
 
 // -S is silent (to avoid printing query progress to stdout)
 shell_exec( "hive -S -e \"$query\" > $outputFile 2> $errorFile" );
@@ -56,5 +63,8 @@ foreach( $resultLines as $lineNumber => $line ) {
 }
 
 foreach( $metrics as $metricName => $value ) {
-	exec( "echo \"$metricName $value `date +%s`\" | nc -q0 graphite.eqiad.wmnet 2003" );
+	$targetDate = $dateYesterday->format( 'Y-m-d' );
+	exec( "echo \"$metricName $value `date -d \"$targetDate\" +%s`\" | nc -q0 graphite.eqiad.wmnet 2003" );
 }
+
+unlink( $outputFile );
