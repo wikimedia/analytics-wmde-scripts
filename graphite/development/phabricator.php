2ca25a9dddf7f717b9b46b56d5a4908d04ba9c9b
addshore <addshorewiki@gmail.com>
2016-02-25 13:01:13 +0000

    Adjust for new version of phabricator
    Bug: T127934
    Change-Id: I49491875ab2e5928a9c22f2d18159d8db1c9dac3

--- graphite/development/phabricator.php
+++ graphite/development/phabricator.php
@@ -17,21 +17,31 @@ class WikidataPhabricator{
 	public function execute() {
 		$response = WikimediaCurl::retryingCurlGet( 'http://phabricator.wikimedia.org/tag/wikidata/', true );
 		$page = $response[1];
-		$cols = array();
 
-		$pageParts = explode( '<span class="phui-header-header">', $page );
-		foreach( $pageParts as $pagePartKey => $pagePart ){
-			if( $pagePartKey < 2 ) {
+		$colNames = array();
+		$colCounts = array();
+
+		$headerParts = explode( '<span class="phui-header-header">', $page );
+		foreach( $headerParts as $headerPartKey => $headerPart ){
+			if( $headerPartKey == 0 ) {
 				continue;
 			}
-			$colParts = explode( '</span>', $pagePart, 2 );
-			$colName = $colParts[0];
-			$colCards = substr_count( $pagePart, 'data-sigil="project-card"' );
-			$cols[$colName] = $colCards;
+			$innerHeaderParts = explode( '</span>', $headerPart, 2 );
+			$colNames[] = $innerHeaderParts[0];
 		}
 
-		foreach( $cols as $name => $value ) {
+		$dataParts = explode( '"columnMaps":', $page );
+		$dataParts = explode( '"orderMaps":', $dataParts[1] );
+		$columnMaps = trim( $dataParts[0], " ," );
+		$columnMaps = json_decode( $columnMaps, true );
+		foreach( $columnMaps as $values ) {
+			$colCounts[] = count( $values );
+		}
+
+		//Note: This makes the assumption that the cols are in the same order in the data fields as on the workboard
+		foreach( $colNames as $key => $name ) {
 			$name = str_replace( ' ', '_', $name );
+			$value = $colCounts[$key];
 			$metricName = 'daily.wikidata.phabricator.board.columns.' . $name;
 			exec( "echo \"$metricName $value `date +%s`\" | nc -q0 graphite.eqiad.wmnet 2003" );
 		}
