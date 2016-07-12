108ba8fb015877f18ba7780d05373c3cd56e89d1
addshore <addshorewiki@gmail.com>
2016-07-12 09:19:46 +0100

    Update path to the db ini file
    Bug: T140064
    Change-Id: Ie369c188251b56be91f333e7f221687ae2c83f61

--- src/WikimediaDb.php
+++ src/WikimediaDb.php
@@ -6,7 +6,8 @@
 class WikimediaDb {
 
 	public static function getPdo() {
-		$sqlConf = parse_ini_file( '/etc/mysql/conf.d/analytics-research-client.cnf' );
+		// This script is controlled by the statistics::wmde module
+		$sqlConf = parse_ini_file( '/etc/mysql/conf.d/research-wmde-client.cnf' );
 
 		$pdo = new PDO(
 			"mysql:host=analytics-store.eqiad.wmnet",
