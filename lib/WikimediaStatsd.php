c8dcfa09b55a2ec9371fe6bdb5a9fff4090c7609
addshore <addshorewiki@gmail.com>
2016-07-13 10:00:43 +0100

    Accept statsd and graphite hosts with ports
    Still fallback to default port if one is not provided.
    
    Bug: T140081
    Change-Id: Ie29be4b7c86000edbd2c06c9267c7c46b108a4e4

--- lib/WikimediaStatsd.php
+++ lib/WikimediaStatsd.php
@@ -6,8 +6,17 @@
 class WikimediaStatsd {
 
 	public static function sendGauge( $metricName, $value ) {
-		$statsdHost = Config::getValue( 'statsd_host' );
-		exec( "echo \"$metricName:$value|g\" | nc -w 1 -u $statsdHost 8125" );
+		list( $host, $port ) = self::getHostAndPort();
+		exec( "echo \"$metricName:$value|g\" | nc -w 1 -u $host $port" );
+	}
+
+	private static function getHostAndPort() {
+		$host = Config::getValue( 'statsd_host' );
+		$port = '8125';
+		if ( strstr( $host, ':' ) ) {
+			return explode( ':', $host );
+		}
+		return array( $host, $port );
 	}
 
 }
