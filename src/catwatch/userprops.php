90239eb27a5e4f936f016d094e1a13a701ac19ec
addshore <addshorewiki@gmail.com>
2016-07-22 14:15:45 +0100

    Actually run second betafeatures query
    Change-Id: Ifd7d87a6710d6e07ea34324f4d671c4f15157a78

--- src/catwatch/userprops.php
+++ src/catwatch/userprops.php
@@ -24,7 +24,7 @@ $pdo = WikimediaDb::getPdo();
 $metrics = array();
 
 foreach( $dbs as $dbname ) {
-	if( $dbname === 'labswiki' || $dbname === 'labtest' ) {
+	if( $dbname === 'labswiki' || $dbname === 'labtestwiki' ) {
 		continue;
 	}
 	$thisWikiHasSomeUse = false;
