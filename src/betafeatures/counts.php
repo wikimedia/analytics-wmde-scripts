ed76e8639f8028e1cc0bf6bd2bed438b74b345cf
addshore <addshorewiki@gmail.com>
2016-07-25 22:30:53 +0100

    Track global user enables & disables of beta features
    This tracks how many global users have enabled / disabled
    a betafeature.
    
    If a user has a feature enabled on a single wiki they are
    counted as using the feature.
    If they then enabled it on a second wiki, this will not count
    as another enable.
    Only if a user removes the feature from all wikis will it count
    as a disable.
    
    Bug: T140226
    Change-Id: Ib3df65f4e28bf2ffa06e21f1d4f89f13f79c6582

--- src/betafeatures/counts.php
+++ src/betafeatures/counts.php
@@ -36,15 +36,25 @@ $dbs = array_filter( $dbs );
 $pdo = WikimediaDb::getPdo();
 
 $metrics = array();
+$tempTableName = 'staging.wmde_analytics_betafeature_users_temp';
+$yesterdayTableName = 'staging.wmde_analytics_betafeature_users_yesterday';
 
-// Create temporary tables
-$sql = "CREATE TEMPORARY TABLE IF NOT EXISTS staging.wmde_analytics_betafeature_users";
+// Create temporary table
+$sql = "CREATE TEMPORARY TABLE IF NOT EXISTS $tempTableName";
 $sql .= "( user_name VARCHAR(255) NOT NULL, feature VARBINARY(255) NOT NULL, PRIMARY KEY (user_name, feature) )";
 $queryResult = $pdo->query( $sql );
 if ( $queryResult === false ) {
-	die( "Failed to create staging.wmde_analytics_betafeature_users" );
+	die( "Failed to create temp table $tempTableName" );
+}
+// Create yesterday table
+$sql = "CREATE TABLE IF NOT EXISTS $yesterdayTableName";
+$sql .= "( user_name VARCHAR(255) NOT NULL, feature VARBINARY(255) NOT NULL, PRIMARY KEY (user_name, feature) )";
+$queryResult = $pdo->query( $sql );
+if ( $queryResult === false ) {
+	die( "Failed to create table $yesterdayTableName" );
 }
 
+// Loop through all wiki databases
 foreach( $dbs as $dbname ) {
 	if( $dbname === 'labswiki' || $dbname === 'labtestwiki' ) {
 		continue;
@@ -62,9 +72,9 @@ foreach( $dbs as $dbname ) {
 		}
 	}
 
-	// Record individuals
+	// Record individuals into the temp table
 	foreach( $currentFeatures as $feature ) {
-		$sql = "INSERT IGNORE INTO staging.wmde_analytics_betafeature_users ( user_name, feature )";
+		$sql = "INSERT IGNORE INTO $tempTableName ( user_name, feature )";
 		$sql .= " SELECT user_name, up_property FROM $dbname.user_properties";
 		$sql .= " JOIN $dbname.user ON up_user = user_id";
 		$sql .= " WHERE up_property = '$feature' AND up_value = '1'";
@@ -75,18 +85,20 @@ foreach( $dbs as $dbname ) {
 	}
 }
 
+// Send total user_counts (1 global user can be counted more than once)
 foreach( $metrics as $featureName => $value ) {
 	if ( in_array( $featureName, $currentFeatures ) && $value > 0 ) {
 		WikimediaGraphite::sendNow( 'daily.betafeatures.user_counts.totals.' . $featureName, $value );
 	}
 }
 
+// Select and send the global user counts (each global user is only counted once)
 $sql = "SELECT COUNT(*) AS count, feature";
-$sql .= " FROM staging.wmde_analytics_betafeature_users";
+$sql .= " FROM $tempTableName";
 $sql .= " GROUP BY feature";
 $queryResult = $pdo->query( $sql );
 if( $queryResult === false ) {
-	Output::timestampedMessage( "SELECT FROM staging.wmde_analytics_betafeature_users FAILED!!" );
+	Output::timestampedMessage( "SELECT FROM temp table $tempTableName FAILED!!" );
 } else {
 	foreach( $queryResult as $row ) {
 		if ( in_array( $row['feature'], $currentFeatures ) && $row['count'] > 0 ) {
@@ -96,4 +108,49 @@ if( $queryResult === false ) {
 		);
 		}
 	}
+}
+
+// Compare todays data with yesterdays data (if present)
+$queryResult = $pdo->query( "SELECT * FROM $yesterdayTableName LIMIT 1" );
+if ( $queryResult === false ) {
+	Output::timestampedMessage( "FAILED: $sql" );
+} else if( count( $queryResult->fetchAll() ) > 0 ) {
+	// Work out what has changed between days
+	// Emulated INTERSECT: http://stackoverflow.com/a/950505/4746236
+	$sql = "SELECT 'enables' AS state, today.* FROM $tempTableName AS today";
+	$sql .= " WHERE ROW(today.user_name, today.feature) NOT IN";
+	$sql .= " ( SELECT * FROM $yesterdayTableName )";
+	$sql .= " UNION ALL";
+	$sql .= " SELECT 'disables' AS state, yesterday.* FROM $yesterdayTableName AS yesterday";
+	$sql .= " WHERE ROW(yesterday.user_name, yesterday.feature) NOT IN";
+	$sql .= " ( SELECT * FROM $tempTableName )";
+	$sql = "SELECT state, COUNT(*) AS count, feature FROM ( $sql ) AS a GROUP BY state, feature";
+	$queryResult = $pdo->query( $sql );
+	if ( $queryResult === false ) {
+		Output::timestampedMessage( "FAILED Intersection, Skipping!!" );
+	} else {
+		foreach( $queryResult as $row ) {
+			WikimediaGraphite::sendNow(
+				'daily.betafeatures.global_' . $row['state'] . '.totals.' . $row['feature'],
+				$row['count']
+			);
+		}
+	}
+} else {
+	Output::timestampedMessage( "No data contained in yesterdays table, Skipping!!" );
+}
+
+// Clear yesterdays table
+$sql = "TRUNCATE TABLE $yesterdayTableName";
+$queryResult = $pdo->query( $sql );
+if( $queryResult === false ) {
+	Output::timestampedMessage( "FAILED: $sql" );
+}
+
+// Add todays data into the yesterday table
+$sql = "INSERT INTO $yesterdayTableName ( user_name, feature )";
+$sql .= " SELECT user_name, feature FROM $tempTableName";
+$queryResult = $pdo->query( $sql );
+if( $queryResult === false ) {
+	Output::timestampedMessage( "FAILED: $sql" );
 }
