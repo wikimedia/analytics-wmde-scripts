b9b9249027c7ce0ebdd3a4f08f72e34269e90483
Amir Sarabadani <ladsgroup@gmail.com>
2018-04-12 19:44:09 +0200

    Track number of constraint definition snaks over time
    Bug: T191891
    Change-Id: Ib4bf1710a0eb33a8fc224373295a261909c77519
    (cherry picked from commit 1d0a167f986c8ddb98d529b010f1fad7dc9f0bca)

---
+++ src/wikidata/sparql/constraintsByType.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+<?php
+
+/**
+ * @author Amir Sarabadani
+ */
+
+require_once( __DIR__ . '/../../../lib/load.php' );
+$output = Output::forScript( 'wikidata-sparql-constraint-snaks' )->markStart();
+$metrics = new WikidataConstraintsByType();
+$metrics->execute();
+$output->markEnd();
+
+class WikidataConstraintsByType {
+
+	public function execute() {
+		$query = <<<EOF
+SELECT ?type (COUNT(DISTINCT ?constraint) AS ?count) WHERE {
+?property a wikibase:Property;
+p:P2302 ?constraint.
+?constraint ps:P2302 ?type. } GROUP BY ?type ?typeLabel
+EOF;
+
+		$response = WikimediaCurl::retryingCurlGet( "https://query.wikidata.org/sparql?format=json&query=" . urlencode( $query ) );
+
+		if( $response === false ) {
+			throw new RuntimeException( "The SPARQL request failed!" );
+		}
+
+		$data = json_decode( $response[1], true );
+
+		foreach( $data['results']['bindings'] as $binding ) {
+			$this->handleBinding( $binding );
+		}
+
+	}
+
+	private function handleBinding( $binding ) {
+		if( !array_key_exists( 'count', $binding ) || !array_key_exists( 'type', $binding ) ) {
+			return;
+		}
+		$constraintCount = $binding['count']['value'];
+		$constraint = str_replace( "http://www.wikidata.org/entity/", '', $binding['type']['value'] );
+		WikimediaGraphite::sendNow( 'daily.wikidata.constraints.byType.' . $constraint, (int)$constraintCount );
+	}
+
+}
