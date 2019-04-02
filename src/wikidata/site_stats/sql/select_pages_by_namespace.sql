eda05d04dedef00286b69988097bee2b504f6503
Marius Hoch <hoo@online.de>
2019-04-02 14:28:29 +0200

    Track number of Schemas
    Wikibase Schemas are in NS640:
    https://www.mediawiki.org/wiki/Extension_default_namespaces#WikibaseSchema
    
    Bug: T216835
    Change-Id: I429c824626794fc155873f7541eb7f83c24a452b

--- src/wikidata/site_stats/sql/select_pages_by_namespace.sql
+++ src/wikidata/site_stats/sql/select_pages_by_namespace.sql
@@ -4,7 +4,8 @@ SELECT
   COUNT(*) AS count
 FROM wikidatawiki.page
 WHERE page_namespace = 0
-OR page_namespace = 120
 OR page_namespace = 1
+OR page_namespace = 120
 OR page_namespace = 146
+OR page_namespace = 640
 GROUP BY page_namespace, page_is_redirect
