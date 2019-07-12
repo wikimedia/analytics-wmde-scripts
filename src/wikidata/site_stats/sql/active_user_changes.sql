ea557f4e2781acd0846017d078396b5552e109e0
Amir Sarabadani <Ladsgroup@gmail.com>
2019-07-12 22:06:25 +0200

    Fix queries to follow the new actor schema
    Bug: T227788
    Change-Id: I186bc65e841b19c3ede924ccf1cf6290029d9ba1
    (cherry picked from commit 6843a5b9c1742c05daa105c77aa0991fe03c7087)

--- src/wikidata/site_stats/sql/active_user_changes.sql
+++ src/wikidata/site_stats/sql/active_user_changes.sql
@@ -1,9 +1,10 @@
 SELECT
-		rc_user_text AS user,
+		actor_name AS user,
 		COUNT( * ) as changes
 	FROM wikidatawiki.recentchanges
-	WHERE rc_user != 0
+	JOIN wikidatawiki.actor ON rc_actor = actor_id
+	WHERE actor_user != 0
 	AND rc_bot = 0
 	AND ( rc_log_type != 'newusers' OR rc_log_type IS NULL)
 	AND UNIX_TIMESTAMP(rc_timestamp) >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 day))
-	GROUP BY rc_user_text;
+	GROUP BY actor_name;
