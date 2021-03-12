8a8e909c4cd6c063aad25ad78e0eb585a9af0689
sihe <silvan.heintze@wikimedia.de>
2021-03-12 17:53:02 +0100

    Track editor numbers split by namespace
    Bug: T275999
    Change-Id: Ie46428498dcdca94ffa0bb704fae078c57fbecbf

---
+++ src/wikidata/site_stats/sql/active_user_changes_by_namespace.sql
@@ -0,0 +1,11 @@
+SELECT
+		actor_name,
+		rc_namespace,
+		COUNT( * ) as changes
+	FROM wikidatawiki.recentchanges
+	JOIN wikidatawiki.actor ON rc_actor = actor_id
+	WHERE actor_user != 0
+	AND rc_bot = 0
+	AND ( rc_log_type != 'newusers' OR rc_log_type IS NULL)
+	AND UNIX_TIMESTAMP(rc_timestamp) >= UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 day))
+	GROUP BY actor_name, rc_namespace;
