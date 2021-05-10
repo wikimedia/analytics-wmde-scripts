965d30cb49cf46f7e7eae383245c8f7707596d38
Amir Sarabadani <Ladsgroup@gmail.com>
2021-05-10 12:44:56 +0200

    Send aggregated entity usage data per dbname and per aspect as well
    The current method, makes grafana query possibly millions of metrics per
    each graph (number of wikis multiplied by aspects with modifiers). This
    is a common misconception and mistake on building metrics.
    
    This patch simply puts all of them in the same bucket.
    
    Bug: T279762
    Change-Id: I7b971a063622ea9615efda67abad29f24891572f

--- src/wikidata/entityUsage.php
+++ src/wikidata/entityUsage.php
@@ -21,20 +21,29 @@ $dbs = explode( "\n", $dblist[1] );
 $dbs = array_filter( $dbs );
 
 $sectionMapper = new WikimediaDbSectionMapper();
-
+$date = date( DATE_ATOM );
+$perAspectValues = [];
 foreach ( $dbs as $dbname ) {
 	$pdo = WikimediaDb::getPdoNewHosts( $dbname, $sectionMapper );
 	// Count each type of entity usage
 	$sql = "SELECT eu_aspect as aspect, count(*) as count FROM $dbname.wbc_entity_usage GROUP BY eu_aspect";
 	$queryResult = $pdo->query( $sql );
+	$perSiteValue = 0;
+	$perSiteMetricName = 'daily.wikidata.entity_usage_per_site.' . $dbname;
 	if ( $queryResult === false ) {
 		$output->outputMessage( "EntityUsage DB query failed for $dbname, Skipping!!" );
 	} else {
 		foreach ( $queryResult as $row ) {
 			$value = $row['count'];
-			$metricName = 'daily.wikidata.entity_usage.' . $dbname . '.' . str_replace( '.', '_', $row['aspect'] );
-			WikimediaGraphite::sendNow( $metricName, $value );
+			$aspectWithModifier = explode( '.', $row['aspect'] );
+			$aspect = $aspectWithModifier[0];
+			$modifierSuffix = isset( $aspectWithModifier[1] ) ? '_' . $aspectWithModifier[1] : '';
+			$metricName = 'daily.wikidata.entity_usage.' . $dbname . '.' . $aspect . $modifierSuffix;
+			WikimediaGraphite::send( $metricName, $value, $date );
+			$perSiteValue += (int)$value;
+			$perAspectValues[$aspect] = ( $perAspectValues[$aspect] ?? 0 ) + (int)$value;
 		}
+		WikimediaGraphite::send( $perSiteMetricName, $perSiteValue, $date );
 	}
 
 	// Count usage (excluding sitelinks) on distinct pages
@@ -46,9 +55,13 @@ foreach ( $dbs as $dbname ) {
 		$queryResult = $queryResult->fetchAll();
 		$metricName = 'daily.wikidata.entity_usage_pages.' . $dbname;
 		$value = $queryResult[0]['pages'];
-		WikimediaGraphite::sendNow( $metricName, $value );
+		WikimediaGraphite::send( $metricName, $value, $date );
 	}
 
 }
 
+foreach ( $perAspectValues as $aspect => $value ) {
+	$perAspectMetricName = 'daily.wikidata.entity_usage_per_aspect.' . $aspect;
+	WikimediaGraphite::send( $perAspectMetricName, $value, $date );
+}
 $output->markEnd();
