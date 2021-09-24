6f19b1fdb458567bc044c94bac6971564e9a9144
Amir Sarabadani <ladsgroup@gmail.com>
2021-09-24 22:20:05 +0200

    Add script to get some data out of wb_changes
    Bug: T291276
    Change-Id: Ie86be8f2789accda28c9d760c0bcdde5fe88bbec
    (cherry picked from commit e002d5e9539fddbbdecaeb4a9659b7de07aea90f)

---
+++ src/wikidata/wb_changes.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+/**
+ * Track Wikidata dispatch metrics from wb_changes table
+ * Used by: https://grafana.wikimedia.org/d/hGFN2TH7z/edit-dispatching-via-jobs
+ */
+
+require_once __DIR__ . '/../../lib/load.php';
+$output = Output::forScript( 'wikidata-wb_changes' )->markStart();
+
+$pdo = WikimediaDb::getPdoNewHosts( WikimediaDb::WIKIDATA_DB, new WikimediaDbSectionMapper() );
+$queryResult = $pdo->query( 'SELECT MAX(change_time) AS max, MIN(change_time) AS min FROM wikidatawiki.wb_changes;' );
+
+if ( $queryResult === false ) {
+	throw new RuntimeException( 'Something went wrong with the db query' );
+}
+
+$rows = $queryResult->fetchAll();
+if ( !$rows ) {
+	throw new RuntimeException( 'Something went wrong with the db query' );
+}
+
+$max = DateTime::createFromFormat( 'YmdHis', $rows[0]['max'] );
+$min = DateTime::createFromFormat( 'YmdHis', $rows[0]['min'] );
+$now = new DateTime();
+WikimediaGraphite::sendNow( 'wikidata.dispatch_job.wb_changes.freshest', $max->diff( $now )->format( 's' ) );
+WikimediaGraphite::sendNow( 'wikidata.dispatch_job.wb_changes.stalest', $min->diff( $now )->format( 's' ) );
+$output->markEnd();
