3202dafeb0d72dea039fe88750a2fa2ac44cd1c7
Michael Große <michael.grosse@wikimedia.de>
2022-07-14 17:38:51 +0200

    Track number of created Lexemes via ui per day
    Bug: T303394
    Change-Id: I7b3e84b1f8fb7678110140c768359c2a54575cec
    (cherry picked from commit dfffb03f4fd7ad291deaa122aeff95bb6223734f)

--- src/wikidata/site_stats/lexemes.php
+++ src/wikidata/site_stats/lexemes.php
@@ -16,6 +16,34 @@ class WikidataLexemes {
 
 	public function execute() {
 		$this->collectLexemePagePropStats();
+		$this->collectLexemesCreatedViaUI();
+	}
+
+	private function collectLexemesCreatedViaUI() {
+		$dateTimeFrom = new DateTime( 'midnight 1 day ago UTC' );
+		$dateTimeTo = new DateTime( 'midnight today UTC' );
+
+		$pdo = WikimediaDb::getPdoNewHosts( WikimediaDb::WIKIDATA_DB, new WikimediaDbSectionMapper() );
+		$pdoStatement = $pdo->prepare( file_get_contents( __DIR__ . '/sql/recent_changes_new_lexemes_via_ui.sql' ) );
+		$queryResult = $pdoStatement->execute(
+			[
+				$dateTimeFrom->format( 'YmdHis' ),
+				$dateTimeTo->format( 'YmdHis' ),
+			]
+		);
+
+		if ( $queryResult === false ) {
+			$output->outputMessage( 'DB query failed:' );
+			$output->outputMessage( var_export( $pdoStatement->errorInfo(), true ) );
+			$output->outputMessage( 'Skipping!' );
+			return;
+		}
+
+		$row = $pdoStatement->fetch( PDO::FETCH_ASSOC );
+		WikimediaGraphite::sendNow(
+			'daily.wikidata.site_stats.lexemes.new.ui',
+			$row['count']
+		);
 	}
 
 	private function collectLexemePagePropStats() {
