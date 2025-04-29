ee8b4590cc9ef7a283b9adf2cde01863afe18ba0
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2025-04-29 11:50:37 +0200

    Don’t send targetDate to Prometheus in minutely metrics
    This is apparently unsustainable (T389344#10775468) and fairly pointless
    anyway, as the date was always the current time (minus one minute).
    
    Bug: T389344
    Change-Id: I6f88295e048a2da12694d1ef22a021e5031cfefa
    (cherry picked from commit 3055d34076ae4b423e56fc5d1173ec0db50e82bc)

--- src/wikidata/recentChanges.php
+++ src/wikidata/recentChanges.php
@@ -115,26 +115,26 @@ class WikidataRc {
 
 		// Send everything to graphite!
 		$this->sendMetric( 'wikidata.rc.edits.total', $counters['total'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_all_total', $counters['total'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_all_total', $counters['total'] );
 		$this->sendMetric( 'wikidata.rc.edits.bot', $counters['bot'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_bot_total', $counters['bot'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_bot_total', $counters['bot'] );
 		$this->sendMetric( 'wikidata.rc.edits.anon', $counters['anon'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_anon_total', $counters['anon'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_anon_total', $counters['anon'] );
 		$this->sendMetric( 'wikidata.rc.edits.new', $counters['new'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_new_total', $counters['new'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_new_total', $counters['new'] );
 		$this->sendMetric( 'wikidata.rc.edits.length', $counters['length'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_length_total', $counters['length'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_length_total', $counters['length'] );
 		$this->sendMetric( 'wikidata.rc.edits.mobile', $counters['mobile'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_mobile_total', $counters['mobile'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_mobile_total', $counters['mobile'] );
 		$this->sendMetric( 'wikidata.rc.edits.maxForAUser', $counters['maxForAUser'], $this->graphiteDateTime );
-		$this->sendMetricToPrometheus( 'wikidata_rc_edits_maxForAUser_total', $counters['maxForAUser'], $this->graphiteDateTime );
+		$this->sendMetricToPrometheus( 'wikidata_rc_edits_maxForAUser_total', $counters['maxForAUser'] );
 		foreach ( $counters['oauth'] as $key => $value ) {
 			$this->sendMetric( "wikidata.rc.edits.oauth.$key", $value, $this->graphiteDateTime );
-			$this->sendMetricToPrometheus( 'wikidata_rc_edits_oauth_total', $value, $this->graphiteDateTime, [ 'key' => $key ] );
+			$this->sendMetricToPrometheus( 'wikidata_rc_edits_oauth_total', $value, [ 'key' => $key ] );
 		}
 		foreach ( $counters['summary'] as $key => $value ) {
 			$this->sendMetric( "wikidata.rc.edits.summary.$key", $value, $this->graphiteDateTime );
-			$this->sendMetricToPrometheus( 'wikidata_rc_edits_summary_total', $value, $this->graphiteDateTime, [ 'key' => $key ] );
+			$this->sendMetricToPrometheus( 'wikidata_rc_edits_summary_total', $value, [ 'key' => $key ] );
 		}
 	}
 
@@ -163,9 +163,7 @@ class WikidataRc {
 		WikimediaGraphite::send( $name, $value, $targetDate );
 	}
 
-	private function sendMetricToPrometheus( $name, $value, DateTime $targetDate, $labels = [] ) {
-		$targetDate = $targetDate->format( 'Y-m-d H:i:s' );
-		$labels['targetDate'] = $targetDate;
+	private function sendMetricToPrometheus( $name, $value, $labels = [] ) {
 		WikimediaStatsdExporter::sendNow( $name, $value, $labels );
 	}
 
