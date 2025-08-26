93b1cc8bfcfee1028b5bf3a9464bac3781ead916
Alexander Vorwerk <zabe@avorwerk.net>
2025-08-26 14:54:51 +0200

    Replace rc_new with rc_source
    Bug: T36320
    Change-Id: I05ccb1dd0239d1a7f9d3e9e9ab0a78dea2cc1f68
    (cherry picked from commit 79211afa3c0d22d88ac1fd887604a45430bbbdbf)

--- src/wikidata/site_stats/sql/recent_changes_new_lexemes_via_ui.sql
+++ src/wikidata/site_stats/sql/recent_changes_new_lexemes_via_ui.sql
@@ -4,7 +4,7 @@ JOIN wikidatawiki.change_tag ON ct_rc_id = rc_id
 JOIN wikidatawiki.change_tag_def ON ctd_id = ct_tag_id
 WHERE rc_timestamp >= ?
 AND rc_timestamp < ?
-AND rc_new = 1
+AND rc_source = 'mw.new'
 AND rc_namespace = 146 /* Lexeme: */
 AND ctd_name = 'wikidata-ui'
 GROUP BY ctd_name
