e194e0eeafa797006bb88992d7e75a99ea649e91
Andrew McAllister <andrew.mcallister@wikimedia.de>
2026-02-24 17:56:25 +0100

    Split active_user_changes.sql into user/temp account versions and run both
    Bug: T416680
    Change-Id: Ie722a7076bedab8259e94d9cc7e7f567e966e1cb
    (cherry picked from commit 3fe959895c55a2c3535d2c8f717e5b1dcd447eea)

---
+++ src/wikidata/site_stats/sql/changes/active_temporary_account_changes.sql
@@ -0,0 +1,15 @@
+-- Note: Temporary accounts; not users or bots.
+SELECT
+    actor_name,
+    count(*) as changes
+FROM wikidatawiki.recentchanges
+JOIN wikidatawiki.actor ON rc_actor = actor_id
+JOIN wikidatawiki.user ON actor_user = user_id
+WHERE
+    actor_user != 0
+    AND user_is_temp
+    AND rc_bot = 0
+    AND (rc_log_type != 'newusers' OR rc_log_type IS NULL)
+    AND rc_timestamp >= date_format(date_sub(now(), INTERVAL 30 day), '%Y%m%d%H%i%s')
+GROUP BY actor_name
+;
